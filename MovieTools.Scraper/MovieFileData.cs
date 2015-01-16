e2e9c8c0a9650935947f9f7ddd663aa0d1be4910
shirbr510 <shirbr510@gmail.com>
2015-01-16 16:44:47 +0200

    Added all source code of project for the meanwhile

---
+++ MovieTools.Scraper/MovieFileData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace MovieTools.Scraper
+{
+    public struct MovieFileData
+    {
+        private static readonly char[] PaddingChars = {'.', '_', '-'};
+
+        public static string[] ValidMovieExtensions = { ".webm", ".mp4", ".wmv", ".mkv", ".flv", ".avi" };
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public string Extension { get; set; }
+
+        public string Resolution { get; set; }
+
+        public string ToFilePath()
+        {
+            var str = Name;
+            if (Year != -1)
+            {
+                str = string.Join(".", str, Year);
+            }
+            if (!string.IsNullOrWhiteSpace(Resolution))
+            {
+                str = string.Join(".", str, Resolution);
+            }
+            return string.Join(".", str, Extension);
+        }
+
+        public string ToDirectoryPath()
+        {
+            return Year == -1 ? Name : string.Format("{0} ({1})", Name, Year);
+        }
+
+        public static MovieFileData Parse(string fileName)
+        {
+            var data = new MovieFileData();
+            var regMatch = Regex.Match(fileName,
+                @"^(?<MovieName>.+)\.(?<Year>\d{4})\.(([^\.]*)\.)*(?<Resolution>\d{3,4}p)\.(([^\.]*)\.)*(?<Extension>[^\.]*)$");
+            if (regMatch.Success)
+            {
+                data.Name = PaddingRemover(regMatch.Groups["MovieName"].Value);
+                int year;
+                data.Year = int.TryParse(regMatch.Groups["Year"].Value, out year) ? year : -1;
+                data.Resolution = regMatch.Groups["Resolution"].Value;
+                data.Extension = regMatch.Groups["Extension"].Value;
+            }
+            else
+            {
+                regMatch = Regex.Match(fileName,
+                    @"^(?<MovieName>.+)\.(([^\.]*)\.)*(?<Extension>[^\.]*)$");
+                data.Name = PaddingRemover(regMatch.Groups["MovieName"].Value);
+                data.Year = -1;
+                data.Resolution = "";
+                data.Extension = regMatch.Groups["Extension"].Value;
+            }
+            return data;
+        }
+
+        private static string PaddingRemover(string value)
+        {
+            var str = value;
+            foreach (var paddingChar in PaddingChars)
+            {
+                var escapeCharacter = Regex.Escape(paddingChar.ToString(CultureInfo.InvariantCulture));
+                str = Regex.Replace(str, escapeCharacter, " ");
+            }
+            return string.Join(" ", str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
---
+++ MovieTools.Scraper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MovieTools.Scraper
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            DoStuff();
+        }
+
+        private static void DoStuff()
+        {
+            const string dirPath = @"F:\Movies\A - Z";
+            var rootDir = new DirectoryInfo(dirPath);
+            foreach (var dirInfo in rootDir.GetDirectories())
+            {
+                foreach (
+                    var file in
+                        dirInfo.GetFiles().Where(fileInfo => MovieFileData.ValidMovieExtensions.Contains(fileInfo.Extension)))
+                {
+                    Console.WriteLine(file.Name);
+                    var data = MovieFileData.Parse(file.Name);
+                    Console.WriteLine(data.ToDirectoryPath());
+                    Console.WriteLine(data.ToFilePath());
+                }
+            }
+        }
+    }
+}
